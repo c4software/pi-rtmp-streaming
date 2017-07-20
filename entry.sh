41d1beda469e50f0de35ac981f8d8ba0a0be8f04
Brosseau Valentin <c4software@gmail.com>
2017-07-20 16:46:26 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -17,12 +17,11 @@ while getopts ":a:p:" opt; do
   esac
 done
 
-SECRET=$1
 W=${W:-"1920"}
 H=${H:-"1080"}
 RTMP=${RTMP:-"rtmp://a.rtmp.youtube.com/live2/"}
 RASPI_EXTRA_ARGS=${RASPI_EXTRA_ARGS:-""}
 
-echo Live-stream secret: $1
+echo Live-stream KEY: $1
 
 raspivid -o - -t 0 -w $W -h $H -fps 24 -g 24 $RASPI_EXTRA_ARGS | ffmpeg -re -f lavfi -i anullsrc -f h264 -i pipe:0 -c:v copy -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -strict experimental -f flv $RTMP$1
