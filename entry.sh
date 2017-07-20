71f35523eba06b9c35c8da418de00b7fdc05b484
Brosseau Valentin <c4software@gmail.com>
2017-07-20 17:07:55 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts ":s:w:h:rtmp:raspi_extra_args:" opt; do
+while getopts ":s:w:h:f:e:" opt; do
   case $opt in
     s) SECRET="$OPTARG"
     ;;
@@ -8,9 +8,9 @@ while getopts ":s:w:h:rtmp:raspi_extra_args:" opt; do
     ;;
     h) H="$OPTARG"
     ;;
-    rtmp) RTMP="$OPTARG"
+    f) RTMP="$OPTARG"
     ;;
-    raspi_extra_args) RASPI_EXTRA_ARGS="$OPTARG"
+    e) RASPI_EXTRA_ARGS="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
@@ -22,6 +22,15 @@ H=${H:-"1080"}
 RTMP=${RTMP:-"rtmp://a.rtmp.youtube.com/live2/"}
 RASPI_EXTRA_ARGS=${RASPI_EXTRA_ARGS:-""}
 
-echo Live-stream KEY: $SECRET
+echo "=============="
+echo "== SETTINGS =="
+echo "=============="
+echo "Live-stream KEY: $SECRET"
+echo "Live-stream SERVER: $RTMP"
+echo "Video: $W x $H"
+echo "Extra Args: $RASPI_EXTRA_ARGS"
+echo "=========="
+echo "== LIVE =="
+echo "=========="
 
 raspivid -o - -t 0 -w $W -h $H -fps 24 -g 24 $RASPI_EXTRA_ARGS | ffmpeg -re -f lavfi -i anullsrc -f h264 -i pipe:0 -c:v copy -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -strict experimental -f flv $RTMP$SECRET
