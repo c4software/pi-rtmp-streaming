3c59b67272966ebea1cac537c8df5277e7691719
Brosseau Valentin <c4software@gmail.com>
2017-07-20 16:06:15 +0200

    Make the url customisable.

--- entry.sh
+++ entry.sh
@@ -3,7 +3,8 @@
 SECRET=$1
 W=${2:-"1920"}
 H=${3:-"1080"}
+RTMP=${4:-"rtmp://a.rtmp.youtube.com/live2/"}
 
 echo Live-stream secret: $1
 
-raspivid -o - -t 0 -w $W -h $H -fps 24 -g 24 | ffmpeg -re -f lavfi -i anullsrc -f h264 -i pipe:0 -c:v copy -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$1
+raspivid -o - -t 0 -w $W -h $H -fps 24 -g 24 | ffmpeg -re -f lavfi -i anullsrc -f h264 -i pipe:0 -c:v copy -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -strict experimental -f flv $RTMP$1
